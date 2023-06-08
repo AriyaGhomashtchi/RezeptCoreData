9f8a29c92c903fd56e8285af735272d911f41c77
Ariya Ghomashtchi <ariya_ghomashtchi@yahoo.com>
2023-06-08 10:20:30 +0200

    Initial Commit

---
+++ RezeptCoreData/Model/RezeptCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  RezeptCoreDataApp.swift
+//  RezeptCoreData
+//
+//  Created by Ariya Ghomashtchi on 08.06.23.
+//
+
+import SwiftUI
+
+@main
+struct RezeptCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RezeptCoreData/ViewModel/RezeptViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RezeptViewModel.swift
+//  RezeptCoreData
+//
+//  Created by Ariya Ghomashtchi on 08.06.23.
+//
+
+import Foundation
